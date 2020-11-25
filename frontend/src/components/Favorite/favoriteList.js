bd248af881ff051d48902b1765c6caa2722896f3
kholoudmohtaseb <kholoud.almohtseb@gmail.com>
2020-11-25 19:20:53 +0200

    favorite list

--- frontend/src/components/Favorite/favoriteList.js
+++ frontend/src/components/Favorite/favoriteList.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./favoriteList.css";
 
-function favoriteList() {
+function FavoriteList() {
     return (
-        <div>
-            
+        <div id='favoriteList'>
+            <p>Favorit trips</p>
+            <table>
+                <tr>
+                    <td><img src='https://upload.wikimedia.org/wikipedia/commons/5/5b/Israel_-_Haifa_-_Bahai_Gardens_004.jpg' alt='img' />
+                        <p>dubai</p></td>
+
+                    <td><img src='https://upload.wikimedia.org/wikipedia/commons/5/5b/Israel_-_Haifa_-_Bahai_Gardens_004.jpg' alt='img' />
+                        <p>dubai</p></td>
+                </tr>
+            </table>
+
+
         </div>
     )
 }
 
-export default favoriteList
+export default FavoriteList
